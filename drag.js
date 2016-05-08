164a9aa77a0e7da79542aa4db13d57a897193a36
lh1212 <492362566@qq.com>
2016-05-08 10:26:40 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -1,3 +1,9 @@
-function drag() {
-	// body...
+function drag(id) {
+	var obj = document.getElementById(id);
+	var disX = 0;
+	var disY = 0;
+	obj.onmousedown = function(ev){
+		disX = ev.pageX - obj.offsetLeft;
+		disY = ev.pageY - obj.offsetTop;
+	}
 }
